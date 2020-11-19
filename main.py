2d39af5d54f08ce8ca18217873f6c57c1a1fe5d9
Desiree3583 <72775978+Desiree3583@users.noreply.github.com>
2020-11-19 13:10:28 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 while True:
-    print("Temperature (F): ")
+    print("Temperature (F): " + input.temperature(TemperatureUnit.FAHRENHEIT))
+    a = 40
+    b = 90
+    if a < b:
+        light.set_pixel_color(5, light.rgb(255, 0, 0))
+    else: 
+        light.set_pixel_color(5, light.rgb(0, 0, 255))
--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+let a: number;
+let b: number;
 while (true) {
-    console.log("Temperature (F): ")
+    console.log("Temperature (F): " + input.temperature(TemperatureUnit.Fahrenheit))
+    a = 40
+    b = 90
+    if (a < b) {
+        light.setPixelColor(5, light.rgb(255, 0, 0))
+    } else {
+        light.setPixelColor(5, light.rgb(0, 0, 255))
+    }
+    
 }
